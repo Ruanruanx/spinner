af882bbcdb771ca8b2a85a3bde25ae93f1435d3a
Ruanruanx <zxin7@uwo.ca>
2022-08-02 20:33:52 +0000

    spinner

---
+++ spinner1.js
@@ -0,0 +1,35 @@
+setTimeout(() => {
+  process.stdout.write('\r|')
+},100)
+
+setTimeout(() => {
+  process.stdout.write('\r/ ')
+},200)
+
+setTimeout(() => {
+  process.stdout.write('\r- ')
+},300)
+
+setTimeout(() => {
+  process.stdout.write('\r\ ')
+},400)
+
+setTimeout(() => {
+  process.stdout.write('\r| ')
+},500)
+
+setTimeout(() => {
+  process.stdout.write('\r/ ')
+},600)
+
+setTimeout(() => {
+  process.stdout.write('\r- ')
+},700)
+
+setTimeout(() => {
+  process.stdout.write('\r| ')
+},800)
+
+setTimeout(() => {
+  process.stdout.write('\r\n ')
+},900)
